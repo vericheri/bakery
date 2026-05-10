633d142007ee6a821828a76e439a83a1c118308b
Aurella Meidista Septiasa <aurellinda703@gmail.com>
2026-05-10 20:50:51 +0700

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+ment (disable in production)
+error_reporting(E_ALL);
+ini_set('display_error', 1);
+
+define('BASE_PATH', __DIR__);
+
+require_once BASE_PATH . '/core/controller.php';
+require_once BASE_PATH . '/controllers/homecontroller.php';
+require_once BASE_PATH . '/models/menu.php';
+
+$controller = new homecontroller();
+$controller->index();
+?>
